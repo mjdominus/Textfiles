440a159d0218b6a044e7fed17ab2401c7374ecdb
Mark Jason Dominus <mjd@ziprecruiter.com>
2014-03-01 10:19:24 -0800

    search for expressions of the type ((10 + 10 - 10) - 6) = 24

---
+++ fours.pl
@@ -0,0 +1,64 @@
+
+use Math::BigRat;
+use Scalar::Util qw(blessed);
+use strict;
+# seen[$n]{$m} records an expression with $n numbers
+# whose value is $m
+my @seen;
+
+my %op = ( '+' => sub { $_[0] + $_[1] },
+           '*' => sub { $_[0] * $_[1] },
+           '-' => sub { $_[0] - $_[1] },
+           '/' => sub { $_[1] != 0 ? $_[0] / $_[1] : undef },
+           );
+
+for (1..13) {
+  $seen[1]{$_} = [Math::BigRat->new($_)];
+}
+
+scan_seen(1, 1);
+scan_seen(1, 2);
+scan_seen(2, 1);
+scan_seen(1, 3);
+scan_seen(2, 2);
+scan_seen(3, 1);
+
+for my $x (@{$seen[4]{100}}) {
+  printf "%s\n", expr_to_string($x);
+}
+
+sub expr_to_string {
+  my ($x) = @_;
+  return "$x" if ref $x eq "Math::BigRat";
+  my ($op, $a, $b) = @$x;
+  return sprintf "( %s $op %s )", expr_to_string($a), expr_to_string($b);
+}
+
+sub scan_seen {
+  my ($a, $b) = @_;  # sizes of subexpressions
+  print STDERR "($a, $b): \n";
+  my $sa = $seen[$a];
+  my $sb = $seen[$b];
+  for my $ka (keys %$sa) {
+#    print STDERR "  $ka\n";
+    for my $kb (keys %$sb) {
+#      print STDERR "    $kb\n";
+      for my $xa (@{$sa->{$ka}}) {
+        for my $xb (@{$sb->{$kb}}) {
+          for my $op (qw(+ - * /)) {
+            expression($a+$b, $op, $xa, $xb, $ka, $kb);
+          }
+        }
+      }
+    }
+  }
+}
+
+sub expression {
+  my ($n, $op, $x1, $x2, $v1, $v2) = @_;
+  my $v = $op{$op}->($v1, $v2);
+  return unless defined $v;
+  push @{$seen[$n]{$v}}, [$op, $x1, $x2];
+}
+
+
